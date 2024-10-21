860022f1c162109809e1a6465c37d28b16194402
RungChungo <155569880+RungChungo@users.noreply.github.com>
2024-10-21 03:53:32 +0000

    Update grub.nix

--- nixos/modules/system/boot/loader/grub/grub.nix
+++ nixos/modules/system/boot/loader/grub/grub.nix
@@ -691,7 +691,7 @@ in
           });
         };
 
-      enableCryptodisk = lib.warn "The ‘enableCryptodiks’ is deprecated. Please use 'cryptodisk.enable instead’ instead." cryptodisk.enable;
+      enableCryptodisk = lib.warn "The ‘enableCryptodiks’ is deprecated. Please use 'cryptodisk.enable instead’ instead." boot.loader.grub.cryptodisk.enable;
 
       forceInstall = mkOption {
         default = false;
